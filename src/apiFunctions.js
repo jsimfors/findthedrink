ca75f83cfea19f43d43eb176e92a7eee7dcfef9a
Isak Vilhelmsson <isakvi@kth.se>
2020-04-08 11:23:05 +0200

    Implementing functions for talking with the api

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,1 +1,27 @@
 //this file will be responsible for talking with the api
+
+ let storesInStockholm = getStoresInCity('stockholm')
+ console.log(storesInStockholm)
+
+function getStoresInCity(city){
+    fetch('https://systembevakningsagenten.se/api/json/2.1/searchStore.json?' + new URLSearchParams({
+        query: city
+    }), {method: "GET"})             
+    .then(resp => resp.json())
+    .catch(function(error) {
+        console.log(error);
+    })
+}
+
+ let storeInventory = getStoreInventory(934)
+ console.log(storeInventory)
+
+function getStoreInventory(storeId){
+    fetch('https://systembevakningsagenten.se/api/json/1.0/inventoryForStore.json?' + new URLSearchParams({
+        id: storeId
+    }), {method: "GET"})             
+    .then(resp => resp.json())
+    .catch(function(error) {
+        console.log(error);
+    })
+}
